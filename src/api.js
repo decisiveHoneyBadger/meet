aba5085421e4eca4cab88f1dc6c29c3292e15d39
decisiveHoneyBadger <k.guevenc@web.de>
2022-06-24 01:22:13 +0200

    added the endpoint for get events

--- src/api.js
+++ src/api.js
@@ -40,7 +40,10 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'YOUR_GET_EVENTS_API_ENDPOINT' + '/' + token;
+    const url =
+      'https://7xvruegljh.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
+      '/' +
+      token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
@@ -55,25 +58,25 @@ export const getEvents = async () => {
 // gets the access token
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
-};
 
-// retrieves the access token from the Lambda functions on the authorization server. Google will then redirect the user back to the app
-const tokenCheck = accessToken && (await checkToken(accessToken));
+  // retrieves the access token from the Lambda functions on the authorization server. Google will then redirect the user back to the app
+  const tokenCheck = accessToken && (await checkToken(accessToken));
 
-if (!accessToken || tokenCheck.error) {
-  await localStorage.removeItem('access_token');
-  const searchParams = new URLSearchParams(window.location.search);
-  const code = await searchParams.get('code');
-  if (!code) {
-    const results = await axios.get(
-      'https://7xvruegljh.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url',
-    );
-    const { authUrl } = results.data;
-    return (window.location.href = authUrl);
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const results = await axios.get(
+        'https://7xvruegljh.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url',
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
   }
-  return code && getToken(code);
-}
-return accessToken;
+  return accessToken;
+};
 
 // removes the code from the URL once It's finished. And checks whether there’s a path, then build the URL with the current path (or build the URL without a path using window.history.pushState()).
 const removeQuery = () => {
