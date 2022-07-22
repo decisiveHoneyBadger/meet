409271ed0076ef542b4e09a44bbf393c0aa3c184
decisiveHoneyBadger <k.guevenc@web.de>
2022-07-22 13:47:28 +0200

    implemented service worker registration for PWA app

--- src/index.js
+++ src/index.js
@@ -1,31 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import * as serviceWorker from './service-worker';
+// import * as serviceWorker from './service-worker';
 import reportWebVitals from './reportWebVitals';
-import { createRoot } from 'react-dom/client';
 import * as atatus from 'atatus-spa';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// atatus.config('d08b0a9bf4c742db916ab4b31bc30996').install();
+atatus.config('d08b0a9bf4c742db916ab4b31bc30996').install();
