40f7558054d05a5c14f03c0feabc6cf43d10231a
decisiveHoneyBadger <k.guevenc@web.de>
2022-07-25 18:30:00 +0200

    added responsive recharts for data visualization

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import NumberOfEvents from './NumberOfEvents';
 import { WarningAlert } from './Alert';
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import WelcomeScreen from './WelcomeScreen';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
 class App extends Component {
   state = {
@@ -51,12 +60,15 @@ class App extends Component {
   async componentDidMount() {
     this.mounted = true;
     const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const tokenStatus = await checkToken(accessToken);
+    const isTokenValid = tokenStatus && !tokenStatus.error;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get('code');
+
     this.setState({
       showWelcomeScreen: !(code || isTokenValid),
     });
+
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
@@ -73,24 +85,47 @@ class App extends Component {
     this.mounted = false;
   }
 
+  // gets the total number of events per city. The function uses the location and events saved in the state from the Google calendar API
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location,
+      ).length;
+      const city = location.split(', ').shift();
+      return { city, number };
+    });
+    return data;
+  };
+
   render() {
-    if (this.state.showWelcomeScreen === undefined)
-      return <div className="App" />;
+    const { locations, numberOfEvents, events } = this.state;
     return (
       <div className="App">
-        {!navigator.onLine && <WarningAlert text={'Offline data'} />}
-        <CitySearch
-          locations={this.state.locations}
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
+        <NumberOfEvents
           updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
         />
-        <NumberOfEvents updateEvents={this.updateNumberOfEvents} />
-        <EventList events={this.state.events} />
-        <WelcomeScreen
-          showWelcomeScreen={this.state.showWelcomeScreen}
-          getAccessToken={() => {
-            getAccessToken();
-          }}
-        />
+        <h4>Events in each city</h4>
+
+        <ResponsiveContainer height={400}>
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+        <EventList events={events} />
       </div>
     );
   }
